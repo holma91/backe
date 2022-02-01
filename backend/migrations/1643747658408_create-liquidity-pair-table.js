01ca328c423410548384c94c8007b11201c8c6a8
Alexander <alexanderholmberg91@gmail.com>
2022-02-01 21:44:59 +0100

    liq pair migration

---
+++ backend/migrations/1643747658408_create-liquidity-pair-table.js
@@ -0,0 +1,30 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+    pgm.sql(`
+        create table liquidity_pair (
+            id serial primary key,
+            chain varchar,
+            dex varchar,
+            pair_id integer,
+            pair_address varchar(42),
+            token0_address varchar(42),
+            token0_name varchar,
+            token0_symbol varchar,
+            token0_decimals varchar,
+            token1_address varchar(42),
+            token1_name varchar,
+            token1_symbol varchar,
+            token1_decimals varchar,
+            unique(chain, pair_address)
+        );
+    `);
+};
+
+exports.down = (pgm) => {
+    pgm.sql(`
+        drop table liquidity_pair;
+    `);
+};
