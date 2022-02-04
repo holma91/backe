1f1ed0172e4326f63402878e4c8d7de373603681
Alexander <alexanderholmberg91@gmail.com>
2022-02-04 17:10:23 +0100

    validation for post requests

--- backend/src/routes/pairs.js
+++ backend/src/routes/pairs.js
@@ -1,6 +1,28 @@
 import express from 'express';
 import PairRepo from '../repos/pairRepo.js';
 import sendNotifications from './utils/sendNotifications.js';
+import schemas from './utils/requestSchemas.js';
+import validator from 'jsonschema';
+const validate = validator.validate;
+
+// var accountSchema = {
+//     // id: '/SimplePerson',
+//     type: 'object',
+//     properties: {
+//         address: { type: 'string' },
+//         account_type: { type: 'string' },
+//     },
+//     required: ['address', 'account_type'],
+//     additionalProperties: false,
+// };
+
+// var account = {
+//     address: '0xblahblahblah',
+//     account_type: 'EOA',
+// };
+
+// v.addSchema(addressSchema, '/SimpleAddress');
+// console.log(validate(account, accountSchema));
 
 const router = express.Router();
 
@@ -28,11 +50,25 @@ router.get('/pairs/:chain/:dex', async (req, res) => {
 });
 
 router.post('/pairs', async (req, res) => {
-    // validate request
+    const validation = validate(req.body, schemas.pair);
+    if (validation.errors.length > 0) {
+        return res.status(400).send({ messages: validation.errors.map((error) => error.stack) });
+    }
+
     // sendNotifications(req.body);
 
     const pair = await PairRepo.add(req.body);
     res.send(pair);
+});
+
+router.post('/test', async (req, res) => {
+    console.log(req.body);
+    const validation = validate(req.body, schemas.pair);
+    if (validation.errors.length > 0) {
+        return res.status(400).send({ messages: validation.errors.map((error) => error.stack) });
+    }
+
+    res.send('success');
 });
 
 export default router;
---
+++ backend/src/routes/utils/requestSchemas.js
@@ -0,0 +1,36 @@
+const schemas = {
+    pair: {
+        type: 'object',
+        properties: {
+            chain: { type: 'string' },
+            dex: { type: 'string' },
+            address: { type: 'string' },
+            token0: {
+                type: { type: 'object' },
+                properties: {
+                    address: { type: 'string' },
+                    name: { type: 'string' },
+                    symbol: { type: 'string' },
+                    decimals: { type: 'string' },
+                },
+                minProperties: 4,
+            },
+            token1: {
+                type: { type: 'object' },
+                properties: {
+                    address: { type: 'string' },
+                    name: { type: 'string' },
+                    symbol: { type: 'string' },
+                    decimals: { type: 'string' },
+                },
+                minProperties: 4,
+            },
+            liquidity: { type: 'number' },
+            liquidityUSD: { type: 'number' },
+            newToken: { type: 'string' },
+        },
+        minProperties: 7,
+    },
+};
+
+export default schemas;
