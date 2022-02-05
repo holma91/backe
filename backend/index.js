31f1fe007f8e1e103f7afd76a8ad7566ffe84ed6
Alexander <alexanderholmberg91@gmail.com>
2022-02-05 14:31:20 +0100

    change db config to separate prod/dev

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,26 @@ import app from './src/app.js';
 import pool from './src/pool.js';
 import 'dotenv/config';
 
-pool.connect({
+const dev = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-})
+};
+
+const prod = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+};
+
+pool.connect(process.env.environment == 'PROD' ? prod : dev)
     .then(() => {
         app().listen(3005, () => {
             console.log('listening on port 3005');
