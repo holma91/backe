7bebc6781c909f2798b027823932285c4d3eeeee
Alexander <alexanderholmberg91@gmail.com>
2022-02-07 19:37:02 +0100

    performance test

--- bots/onchain/test.js
+++ bots/onchain/test.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 import ethers from 'ethers';
-import { getAccount } from './utils/utils.js';
+import { getAccount, getTokenMetadata } from './utils/utils.js';
 
 const tokenInfoABI = [
     'function name() view returns (string)',
@@ -20,12 +20,17 @@ const account = getAccount('http', 'ETH');
 // }
 // console.timeEnd('runningTime');
 
+// console.time('runningTime');
+// for (let i = 0; i < 10; i++) {
+//     let token = {};
+//     let contract = new ethers.Contract('0x6b175474e89094c44da98b954eedeac495271d0f', tokenInfoABI, account);
+//     let promises = [contract.name(), contract.symbol(), contract.decimals()];
+//     [token.name, token.symbol, token.decimals] = await Promise.all(promises);
+//     console.log(token);
+// }
 console.time('runningTime');
 for (let i = 0; i < 10; i++) {
-    let token = {};
-    let contract = new ethers.Contract('0x6b175474e89094c44da98b954eedeac495271d0f', tokenInfoABI, account);
-    let promises = [contract.name(), contract.symbol(), contract.decimals()];
-    [token.name, token.symbol, token.decimals] = await Promise.all(promises);
-    console.log(token);
+    let metadata = await getTokenMetadata('0x6b175474e89094c44da98b954eedeac495271d0f', account);
+    console.log(metadata);
 }
 console.timeEnd('runningTime');
--- bots/onchain/testListener.js
+++ bots/onchain/testListener.js
@@ -1,19 +1,71 @@
 import { ethers } from 'ethers';
-import { getAccount, onPairCreated } from './utils/utils.js';
 
 const addresses = {
     factory: '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73',
 };
 
-const account = getAccount('http', 'BSC');
+let provider = new ethers.providers.JsonRpcProvider('https://bsc-dataseed.binance.org/');
 
 const factory = new ethers.Contract(
     addresses.factory,
     ['event PairCreated(address indexed token0, address indexed token1, address pair, uint)'],
-    account
+    provider
 );
 
-factory.on('PairCreated', async (token0, token1, addressPair) => {
+const onPairCreated = async (provider, token0Address, token1Address, addressPair) => {
+    console.time(`${addressPair}`);
+
+    console.time(`getTokenMetadata(${token0Address})`);
+    let token0 = await getTokenMetadata(token0Address, provider);
+    console.timeEnd(`getTokenMetadata(${token0Address})`);
+
+    console.time(`getTokenMetadata(${token1Address})`);
+    let token1 = await getTokenMetadata(token1Address, provider);
+    console.timeEnd(`getTokenMetadata(${token1Address})`);
+
+    console.timeEnd(`${addressPair}`);
+
+    console.log(token0);
+    console.log(token1);
+};
+
+const getTokenMetadata = async (tokenAddress, provider) => {
+    let token = {
+        address: tokenAddress,
+    };
+
+    const tokenInfoABI = [
+        'function name() view returns (string)',
+        'function symbol() view returns (string)',
+        'function decimals() public view returns (uint8)',
+    ];
+
+    let success = false;
+    let count = 0;
+
+    while (!success) {
+        try {
+            count++;
+            let contract = new ethers.Contract(token.address, tokenInfoABI, provider);
+            // the 3 lines below could be optimized with Promise.all
+            token.name = await contract.name();
+            token.symbol = await contract.symbol();
+            token.decimals = await contract.decimals();
+            token.deployerAddress = contract.address;
+            success = true;
+        } catch (e) {
+            // try 10 times
+            console.log(e);
+            console.log(`sleeping at ${token.address}`);
+            await sleep(1000);
+            if (count > 10) break;
+        }
+    }
+
+    return token;
+};
+
+factory.on('PairCreated', async (token0Address, token1Address, addressPair) => {
     console.log(`new pair: ${addressPair}`);
-    await onPairCreated(account, token0Address, token1Address, addressPair, 'BSC', 'pancakeswap', knownTokens);
+    await onPairCreated(provider, token0Address, token1Address, addressPair);
 });
