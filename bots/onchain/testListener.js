33b56888dbf1638a765c09100cc6d845ce4da1c7
Alexander <alexanderholmberg91@gmail.com>
2022-02-07 17:17:28 +0100

    test performance

---
+++ bots/onchain/testListener.js
@@ -0,0 +1,25 @@
+import { ethers } from 'ethers';
+import { getAccount } from './utils/utils.js';
+
+const addresses = {
+    factory: '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73',
+};
+
+const account = getAccount('http', 'BSC');
+
+const factory = new ethers.Contract(
+    addresses.factory,
+    ['event PairCreated(address indexed token0, address indexed token1, address pair, uint)'],
+    account
+);
+
+factory.on('PairCreated', async (token0, token1, addressPair) => {
+    console.log(`
+    ~~~~~~~~~~~~~~~~~~
+    New pair detected
+    ~~~~~~~~~~~~~~~~~~
+    token0: ${token0}
+    token1: ${token1}
+    addressPair: ${addressPair}
+    `);
+});
