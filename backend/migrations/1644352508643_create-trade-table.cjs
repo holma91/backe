2cbfafcdbf21fa4036311e09738a38752b493c66
Alexander <alexanderholmberg91@gmail.com>
2022-02-08 21:56:49 +0100

    lp and trade sql migrations

---
+++ backend/migrations/1644352508643_create-trade-table.cjs
@@ -0,0 +1,29 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+    pgm.sql(`
+        create type trade_choice as enum ('buy', 'sell');
+        create table trade (
+            id serial primary key,
+            chain varchar,
+            sender_address varchar(42) references account(address),
+            pair_address varchar(42),
+            token_address varchar(42),
+            token_price numeric,
+            amount numeric,
+            on_coingecko boolean,
+            trade_timestamp timestamp,
+            trade_order trade_choice,
+            foreign key (chain, pair_address) references liquidity_pair(chain, pair_address)
+        );
+    `);
+};
+
+exports.down = (pgm) => {
+    pgm.sql(`
+        drop table trade;
+        drop type trade_choice;
+    `);
+};
