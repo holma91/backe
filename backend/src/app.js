acf83eaa2fddc188c0a58af51168c2eded4131ed
Alexander <alexanderholmberg91@gmail.com>
2022-02-22 13:03:54 +0100

    enable cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import accountsRouter from './routes/accounts.js';
 import pairsRouter from './routes/pairs.js';
 import tokensRouter from './routes/tokens.js';
@@ -8,6 +9,7 @@ const app = () => {
     const app = express();
 
     app.use(express.json());
+    app.use(cors());
     app.use(accountsRouter);
     app.use(tokensRouter);
     app.use(pairsRouter);
