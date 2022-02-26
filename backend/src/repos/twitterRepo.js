183fc06e1589cb098f4489cb1a4cea89d038d206
Alexander <alexanderholmberg91@gmail.com>
2022-02-26 14:21:46 +0100

    add list of followers in response

--- backend/src/repos/twitterRepo.js
+++ backend/src/repos/twitterRepo.js
@@ -2,20 +2,35 @@ import pool from '../pool.js';
 import toCamelCase from './utils/toCamelCase.js';
 
 class TwitterRepo {
-    static async find(onlyRanked) {
-        let res;
-        if (onlyRanked === 'yes') {
-            res = await pool.query('select * from twitter_account where rank = 1;');
-        } else {
-            res = await pool.query('select * from twitter_account;');
-        }
+    static async find() {
+        const res = await pool.query('select * from twitter_account;');
+
         return toCamelCase(res.rows);
     }
 
-    static async findByUsername(username) {
-        const res = await pool.query('select * from twitter_account where username = $1;', [username]);
+    static async findByUsername(username, includeFollowers) {
+        let res;
+        if (includeFollowers === 'yes') {
+            res = await pool.query(
+                `
+                select ta.username, ta.rank, ta.twitter_id, tc.follower_id 
+                from twitter_account ta 
+                join twitter_connection tc on tc.followee_id = ta.twitter_id
+                where ta.username = $1;
+            `,
+                [username]
+            );
+        } else {
+            res = await pool.query('select * from twitter_account where username = $1;', [username]);
+        }
 
-        return toCamelCase(res.rows)[0];
+        let rows = toCamelCase(res.rows);
+        return {
+            username: rows[0].username,
+            rank: rows[0].rank,
+            twitterId: rows[0].twitterId,
+            followers: rows.map((row) => row.followerId),
+        };
     }
 }
 
--- backend/src/routes/twitter.js
+++ backend/src/routes/twitter.js
@@ -4,7 +4,7 @@ import TwitterRepo from '../repos/TwitterRepo.js';
 const router = express.Router();
 
 router.get('/twitter/accounts', async (req, res) => {
-    const accounts = await TwitterRepo.find(req.query['only-ranked']);
+    const accounts = await TwitterRepo.find();
 
     res.send(accounts);
 });
@@ -12,13 +12,19 @@ router.get('/twitter/accounts', async (req, res) => {
 router.get('/twitter/accounts/:username', async (req, res) => {
     const { username } = req.params;
 
-    const account = await TwitterRepo.findByUsername(username);
+    const account = await TwitterRepo.findByUsername(username, req.query['include_followers']);
 
     if (account) {
         res.send(account);
     } else {
         res.sendStatus(404);
     }
+});
+
+router.get('/twitter/accounts', async (req, res) => {
+    const accounts = await TwitterRepo.find(req.query['only-ranked']);
+
+    res.send(accounts);
 });
 
 export default router;
