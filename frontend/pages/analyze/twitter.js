28afeb53b65be2f29a6484639a87b332e9db0905
Alexander <alexanderholmberg91@gmail.com>
2022-03-17 16:24:58 +0100

    add text to twatter page

--- frontend/pages/analyze/twitter.js
+++ frontend/pages/analyze/twitter.js
@@ -47,10 +47,34 @@ const TextCard = () => {
     return (
         <div className="text-card">
             <p className="font-bold">how?</p>
+            <p className="pl-4">1. 100 accounts are handpicked that are deemed "good"</p>
+            <p className="pl-4">
+                2. a list is created with all these accounts plus all the accounts they follow (around 15k accounts in
+                total)
+            </p>
+            <p className="pl-4">3. all their previous tweets the last 7 days are retrieved and saved into a hashmap</p>
+            <p className="pl-4">
+                4. now it's possible to easily see what has been mentioned by somewhat respectable accounts during the
+                last 7 days
+            </p>
+            <br />
             <p>
-                first of all an address needs to be found, and that can be done by for example looking at volume,
-                looking at early investors in successful tokens, taking help of nansen.ai etc. when an address is found,
-                the analysis goes like this:
+                There are some problems, mainly the fact that the twitter API only lets you retrieve data from the last
+                7 days if you don't have a "research account" which from the looks of it is difficult to get. Could get
+                around by running this script once a week and save up the data from now on, but the historical data
+                would then of course never be included. The 100 or so handpicked twitter accounts can be found here
+                <a
+                    className="ml-1 text-blue-600 hover:text-blue-400"
+                    href="https://github.com/holma91/backe/tree/main/analysis/handpicked_twtaccs.json"
+                    target={'_blank'}
+                >
+                    github.com/holma91/backe/tree/main/analysis/handpicked_twtaccs.json
+                </a>{' '}
+            </p>
+            <p className="font-bold">why?</p>
+            <p>
+                If the historical data is somehow retrievable, I think you could do some interesting analysis if you
+                combine the twitter mentions together with market data.
             </p>
         </div>
     );
