0059d5839c24c7cba0a43b609ee4c94423e2f5f1
Alexander <alexanderholmberg91@gmail.com>
2022-03-18 16:47:46 +0100

    fetch

--- frontend/hooks/useFetch.js
+++ frontend/hooks/useFetch.js
@@ -1,10 +1,10 @@
 import useSWR from 'swr';
+// import 'dotenv/config';
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function useFetch(path) {
     const { data, error } = useSWR(`http://localhost:3005/${path}`, fetcher);
-
     return {
         data,
         isLoading: !error && !data,
