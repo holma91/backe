7d4ab489dfa0f0e04fff06e9bb21529a22a2460e
Alexander <alexanderholmberg91@gmail.com>
2022-03-18 18:11:40 +0100

    env var in useFetch

--- frontend/hooks/useFetch.js
+++ frontend/hooks/useFetch.js
@@ -4,7 +4,7 @@ import useSWR from 'swr';
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function useFetch(path) {
-    const { data, error } = useSWR(`http://localhost:3005/${path}`, fetcher);
+    const { data, error } = useSWR(`${process.env.api_url}${path}`, fetcher);
     return {
         data,
         isLoading: !error && !data,
