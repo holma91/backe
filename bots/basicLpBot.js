43c7133ec86582201900a57ddc9e312e06dd0053
Alexander <alexanderholmberg91@gmail.com>
2022-04-02 09:58:46 +0200

    deploy

--- bots/basicLpBot.js
+++ bots/basicLpBot.js
@@ -5,6 +5,8 @@ const provider = getProvider('http', 'DFK');
 const factoryContractAddress = '0x794C07912474351b3134E6D6B3B7b3b4A07cbAAa';
 const crystalvaleFactory = new ethers.Contract(factoryContractAddress, uniV2Factory, provider);
 
+console.log('dfk crystalvale listener activated');
+
 crystalvaleFactory.on('PairCreated', async (token0Address, token1Address, addressPair) => {
     console.log(`
     ~~~~~~~~~~~~~~~~~~
